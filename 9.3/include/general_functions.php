2ff52409e9a65d79a26f2c2ffe499473a9e17f01
abaxter <abaxter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2020-08-13 14:59:26 +0000

    FIX: [9.3]Fixes for automated error reports[t24338][t24015][t24264]
    git-svn-id: https://svn.resourcespace.com/svn/rs/releases@15667 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- 9.3/include/general_functions.php
+++ 9.3/include/general_functions.php
@@ -1585,7 +1585,7 @@ function is_process_lock($name)
     
     # No lock file? return false
     if (!file_exists(get_temp_dir() . "/process_locks/" . $name)) {return false;}
-    
+    if (!is_readable(get_temp_dir() . "/process_locks/" . $name)) {return true;} // Lock exists and cannot read it so must assume it's still valid
     $time=trim(file_get_contents(get_temp_dir() . "/process_locks/" . $name));
     if ((time() - (int) $time)>$process_locks_max_seconds) {return false;} # Lock has expired
     
@@ -1594,7 +1594,6 @@ function is_process_lock($name)
     
 function set_process_lock($name)
     {
-    # Set a process lock
     file_put_contents(get_temp_dir() . "/process_locks/" . $name,time());
     // make sure this is editable by the server in case a process lock could be set by different system users
     chmod(get_temp_dir() . "/process_locks/" . $name,0777);
@@ -2607,7 +2606,7 @@ function validate_html($html)
 * 
 * @return string
 */
-function generateURL($url, $parameters = array(), $set_params = array())
+function generateURL($url, array $parameters = array(), array $set_params = array())
     {
     foreach($set_params as $set_param => $set_value)
         {
--- 9.3/pages/search.php
+++ 9.3/pages/search.php
@@ -570,7 +570,8 @@ $rowstoretrieve = $per_page+$offset;
 if(($k=="" || $internal_share_access) && strpos($search,"!")===false && $archive_standard)
     {
     $collections=do_collections_search($search,$restypes,0,$order_by,$sort,$rowstoretrieve);
-    $colcount = count($collections);
+    try {$colcount = count($collections);}
+    catch (Exception $e) {$colcount = 0;}
     $resourcestoretrieve = max(($rowstoretrieve-$colcount),0);
     }
 else
