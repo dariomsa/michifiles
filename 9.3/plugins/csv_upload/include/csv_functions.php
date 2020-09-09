0a08334c6636568ab8ca33f98071f79b8ae02d75
tpiechowicz <tpiechowicz@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2020-09-09 13:06:12 +0000

    FIX: [9.3] Use status default / access default if specified column not found in csv line. [t24186]
    git-svn-id: https://svn.resourcespace.com/svn/rs/releases@15848 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- 9.3/plugins/csv_upload/include/csv_functions.php
+++ 9.3/plugins/csv_upload/include/csv_functions.php
@@ -239,7 +239,7 @@ function csv_upload_process($filename,&$meta,$resource_types,&$messages,$max_err
         else
             {
             // Get status to set
-            if($csv_set_options["status_column"] != "")
+            if($csv_set_options["status_column"] != "" && in_array($csv_set_options["status_column"],array_keys($line)))
                 {
                 $setstatus = $line[$csv_set_options["status_column"]];
                 if (!is_numeric($setstatus))
@@ -254,7 +254,7 @@ function csv_upload_process($filename,&$meta,$resource_types,&$messages,$max_err
                 }
 
             // Get access to set
-            if($csv_set_options["access_column"] != "")
+            if($csv_set_options["access_column"] != "" && in_array($csv_set_options["access_column"],array_keys($line)))
                 {
                 $setaccess = $line[$csv_set_options["access_column"]];
                 if (!is_numeric($setaccess))
