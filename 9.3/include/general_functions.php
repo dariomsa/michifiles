c9e3f22d74de12428df38a32089922c5637e1b29
jhardy <jhardy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2020-09-09 15:36:41 +0000

    FIX: [9.3] Multiple changes made to general.php and plugins/system_reset/pages/reset.php to fix auto generated php errors  [t24859][t24491][t25022]
    git-svn-id: https://svn.resourcespace.com/svn/rs/releases@15855 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- 9.3/include/general_functions.php
+++ 9.3/include/general_functions.php
@@ -1087,7 +1087,6 @@ function send_mail_phpmailer($email,$subject,$message="",$from="",$reply_to="",$
     // use an external SMTP server? (e.g. Gmail)
     if ($use_smtp) {
         $mail->IsSMTP(); // enable SMTP
-        $mail->SMTPDebug = $debug_log ? 1 : 0;  // debugging: 1 = errors and messages, 2 = messages only
         $mail->SMTPAuth = $smtp_auth;  // authentication enabled/disabled
         $mail->SMTPSecure = $smtp_secure; // '', 'tls' or 'ssl'
         $mail->Host = $smtp_host; // hostname
@@ -1750,9 +1749,15 @@ function get_temp_dir($asUrl = false,$uniqid="")
     if ($uniqid!=""){
         $uniqid=str_replace("../","",$uniqid);//restrict to forward-only movements
         $result.="/$uniqid";
-        if(!is_dir($result)){
+        if(!is_dir($result))
+            {
             // If it does not exist, create it.
-            mkdir($result, 0777,true);
+            try {
+                mkdir($result, 0777,true);
+            } 
+            catch (Exception $e) {
+                debug("get_temp_dir: Attempt to create folder '$result' failed. Reason: {$e->getMessage()}");  
+            }
         }
     }
     
