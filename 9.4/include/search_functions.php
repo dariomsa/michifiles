c47e47cfdf574d0224dfd6f453231390be068979
nbraddy <nbraddy@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2021-01-11 11:34:43 +0000

    FIX: [9.4] Amended SQL for search_public_collections() when $exclude_themes=true to include collections that may not yet have resources [t26656]
    git-svn-id: https://svn.resourcespace.com/svn/rs/releases@16710 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- 9.4/include/search_functions.php
+++ 9.4/include/search_functions.php
@@ -1780,7 +1780,7 @@ function get_default_search_states()
     }
 
 /**
-* Get the required search filter sql for the given ilterfor use in do_search()
+* Get the required search filter sql for the given filter for use in do_search()
 *  
 * @return array
 */
