9c529cfbf59b9557c920368052ad13d4acfbc49e
jnorman <jnorman@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2023-03-16 10:05:21 +0000

    FIX: [10.0] Properly check array for values in array [t32457]
    git-svn-id: https://svn.resourcespace.com/svn/rs/releases@21588 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- 10.0/include/resource_functions.php
+++ 10.0/include/resource_functions.php
@@ -6948,7 +6948,7 @@ function get_resource_all_image_sizes($ref)
             {
             $size_id = trim($size_data["id"]) === "" ? "original" : $size_data["id"];
 
-            if(array_key_exists($size_id, $all_image_sizes))
+            if(array_key_exists("{$size_id}_{$size_data["extension"]}", $all_image_sizes))
                 {
                 continue;
                 }
