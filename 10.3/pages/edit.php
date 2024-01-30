476e1572511112b484e1b1e837f7f19d7dba5277
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-01-30 14:15:57 +0000

    FIX: tabs_on_edit missing fields when changing resource type during refinement [t35019][CR 2663][10.3]
    git-svn-id: https://svn.resourcespace.com/svn/rs/releases@23453 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- 10.3/pages/edit.php
+++ 10.3/pages/edit.php
@@ -1716,8 +1716,8 @@ if ($ref < 0 && !$upload_review_mode)
     }
 
 $fields=get_resource_field_data($use,$multiple,!hook("customgetresourceperms"),$originalref,"",$tabs_on_edit);
-$edit_valid_fields = get_resource_type_fields($resource['resource_type']);
-$edit_valid_fields = array_column($edit_valid_fields,"ref");
+$resource = get_resource_data($ref, false); # By this point the resource type might've been changed
+$edit_valid_fields = array_column(get_resource_type_fields($resource['resource_type']), 'ref');
 
 # Only include fields whose resource type is global or is present in the resource(s) being edited
 if ($multiple) 
