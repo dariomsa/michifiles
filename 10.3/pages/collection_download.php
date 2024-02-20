a8274179afdc06e4388c3a55df789f1a393ec703
abaxter <abaxter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-02-20 09:30:16 +0000

    FIX: [10.3][CR2696] Add use_error_exception to rmdir in collection_download.php[t35150]
    git-svn-id: https://svn.resourcespace.com/svn/rs/releases@23657 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- 10.3/pages/collection_download.php
+++ 10.3/pages/collection_download.php
@@ -603,6 +603,7 @@ if ($submitted != "")
 	//unlink($progress_file);
 	if ($use_zip_extension)
 		{
+        $GLOBALS["use_error_exception"]=true;
         try {
             rmdir(get_temp_dir(false,$id));
             }
@@ -610,6 +611,7 @@ if ($submitted != "")
             {
             debug("collection_download: Attempt delete temp folder failed. Reason: {$e->getMessage()}");
             }
+        unset($GLOBALS["use_error_exception"]);
         }
     collection_log($collection, LOG_CODE_COLLECTION_COLLECTION_DOWNLOADED, "", $size);
     hook('beforedownloadcollectionexit');
