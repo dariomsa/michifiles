e76a7dc0eb2cc569aeef1ba465f4ce6c3f08fd52
mbazan <mbazan@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-03-11 16:09:42 +0000

    FIX: [10.3] Eliminate logging error due to an array key error when attempting to do an upload here following an advanced search [t35283][CR 2741]
    git-svn-id: https://svn.resourcespace.com/svn/rs/releases@23844 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- 10.3/include/resource_functions.php
+++ 10.3/include/resource_functions.php
@@ -4402,7 +4402,7 @@ function update_resource_type($ref,$type)
         {
         $rts = get_resource_types("$type,$old_rt");
         $rts = array_column($rts, 'name', 'ref');
-        resource_log($ref, '', null, $lang["log-rtchange"], $rts[$old_rt], $rts[$type]);
+        resource_log($ref, '', null, $lang["log-rtchange"], $rts[$old_rt]??"", $rts[$type]??"");
         }
     return true;
     }
