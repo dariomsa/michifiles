c59f850b9229ba7302f2fcdb65faaf6b99ed7a98
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-04-25 08:05:25 +0000

    FIX: patching with r24193 and r24213 [CR 2811][t35437][10.3]
    git-svn-id: https://svn.resourcespace.com/svn/rs/releases@24220 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- 10.3/include/resource_functions.php
+++ 10.3/include/resource_functions.php
@@ -23,7 +23,7 @@ $GLOBALS['get_resource_path_fpcache'] = array();
 * @param boolean $scramble         Set to TRUE to get the scrambled folder (requires scramble key for it to work)
 * @param integer $page             For documents, use the page number we are trying to get the preview of.
 * @param boolean $watermarked      Get the watermark version?
-* @param string  $file_modified    Specify when the file was last modified
+* @param int     $file_modified    Specify when the file was last modified as a Unix timestamp
 * @param integer $alternative      ID of the alternative file
 * @param boolean $includemodified  Show when the file was last modified
 *
@@ -48,7 +48,19 @@ function get_resource_path(
     $migrating_scrambled, $scramble_key_old, $filestore_evenspread, $filestore_migrate,
     $baseurl, $k, $get_resource_path_extra_download_query_string_params;
 
-    # returns the correct path to resource $ref of size $size ($size==empty string is original resource)
+    if(isset($resource_path_pull_cache[$ref]) && strtolower((string)$extension) == 'jpg')
+        {
+        $ref = $resource_path_pull_cache[$ref]["ref"];
+        }
+
+    $size = safe_file_name((string) $size);
+    $extension = safe_file_name((string) $extension);
+
+    if (!is_int_loose($file_modified)) {
+        $file_modified = '';
+    }
+
+    # Returns the correct path to resource $ref of size $size ($size==empty string is original resource)
     # If one or more of the folders do not exist, and $generate=true, then they are generated
     if(!preg_match('/^[a-zA-Z0-9]+$/',(string) $extension))
         {
