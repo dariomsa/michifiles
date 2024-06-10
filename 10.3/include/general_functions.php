5bbec7197a1ee47a5fe05ffa84ff8d04ef4e720d
dhuby <dhuby@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2024-06-10 14:02:14 +0000

    CHG: Go live with r24516 - disk usage is never critical [CR 2870]
    git-svn-id: https://svn.resourcespace.com/svn/rs/releases@24517 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- 10.3/include/general_functions.php
+++ 10.3/include/general_functions.php
@@ -4815,7 +4815,7 @@ function get_system_status()
         $used = get_total_disk_usage(); # Total usage in bytes
         $percent = ceil(((int) $used / $avail) * 100);
 
-        if($percent >= 95 && $percent < 99)
+        if($percent >= 95)
             {
             $return['results']['quota_limit'] = [
                 'status' => 'FAIL',
@@ -4825,28 +4825,6 @@ function get_system_status()
                 'severity_text' => $GLOBALS["lang"]["severity-level_" . SEVERITY_WARNING],
             ];
             ++$fail_tests;
-            }
-        else if($percent >= 99 && $percent < 100)
-            {
-            $return['results']['quota_limit'] = [
-                'status' => 'FAIL',
-                'info' => $percent . '% used - nearly full.',
-                'avail' => $avail, 'used' => $used, 'percent' => $percent,
-                'severity' => SEVERITY_CRITICAL,
-                'severity_text' => $GLOBALS["lang"]["severity-level_" . SEVERITY_CRITICAL],
-            ];
-            return $return;
-            }
-        else if($percent >= 100)
-            {
-            $return['results']['quota_limit'] = [
-                'status' => 'FAIL',
-                'info' => $percent . '% used - over quota.',
-                'avail' => $avail, 'used' => $used, 'percent' => $percent,
-                'severity' => SEVERITY_CRITICAL,
-                'severity_text' => $GLOBALS["lang"]["severity-level_" . SEVERITY_CRITICAL],
-            ];
-            return $return;
             }
         else
             {
