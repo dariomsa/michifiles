2e2b81a6f5f815c728f8203e3209b1a7ddb5e366
abaxter <abaxter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2025-01-27 16:44:58 +0000

    FIX: [10.5][CR3188] Appending mapping values adding prefix even if no existing value [t36831]
    git-svn-id: https://svn.resourcespace.com/svn/rs/releases@25961 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- 10.5/pages/tools/staticsync.php
+++ 10.5/pages/tools/staticsync.php
@@ -656,7 +656,13 @@ function ProcessFolder($folder)
                                                         ]))
                                                     {
                                                     $existing_value  = get_data_by_field($r,$field);
-                                                    $values_to_add[$field] = $existing_value . ($staticsync_extension_mapping_append_separator ?? " ") . $value;
+                                                    if ((string) $existing_value != "") {
+                                                    $values_to_add[$field] = $existing_value .
+                                                        ($staticsync_extension_mapping_append_separator ?? " ") .
+                                                        $value;
+                                                    } else {
+                                                        $values_to_add[$field] = $value;
+                                                    }
                                                     }
                                                 }
                                             }
