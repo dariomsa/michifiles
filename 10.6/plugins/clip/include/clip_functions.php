e4e2d83dd3a7782f43639439eaa609f04633ff4e
jnorman <jnorman@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2025-07-31 09:58:25 +0000

    FIX: [10.6] Stop clip_tag from running if no vector is generated or if the vector cannot be retrieved. Correct documentation for clip_generate_vector [t37793] [CR 3456]
    git-svn-id: https://svn.resourcespace.com/svn/rs/releases@27161 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- 10.6/plugins/clip/include/clip_functions.php
+++ 10.6/plugins/clip/include/clip_functions.php
@@ -91,10 +91,9 @@ function vector_visualise(array $vector): string
  *
  * @param int $ref The resource ID for which to generate the vector.
  *
- * @return array|false Returns the ID of the stored vector row on success,
- *                     or false on failure (e.g., file missing or vector generation error).
+ * @return bool        Returns the true on success, or false on failure (e.g., file missing or vector generation error).
  */
-function clip_generate_vector($ref)
+function clip_generate_vector($ref) : bool
 {
     $resource = get_resource_data($ref);
     $ext = "jpg";
@@ -199,10 +198,13 @@ function clip_tag(int $resource)
                 'top_k' => $clip_keyword_count,
         ]);
         $response = curl_exec($ch);
+        $response_status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         logScript ("CLIP service response: " . $response);
         curl_close($ch);
 
-        if (strlen($response)==0) { return false; } // CLIP server unresponsive
+        if (strlen($response) == 0 || $response_status !== 200) { 
+            return false; // CLIP server unresponsive
+        }
 
         foreach (json_decode($response) as $result) {
             # Create new or fetch existing node
