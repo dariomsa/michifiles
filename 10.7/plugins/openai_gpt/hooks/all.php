4c3af28f140d9fd308c0761a5b8722f9ab0c9269
tpiechowicz <tpiechowicz@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2025-11-12 15:43:13 +0000

    FIX: [10.7] Add openai_gpt plugin token usage to get_system_status(). [CR3603][t38483]
    git-svn-id: https://svn.resourcespace.com/svn/rs/releases@27751 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- 10.7/plugins/openai_gpt/hooks/all.php
+++ 10.7/plugins/openai_gpt/hooks/all.php
@@ -240,3 +240,23 @@ function HookOpenai_gptAllAfterpreviewcreation(int $ref, int $alternative, bool 
         }
     return $success[$ref] ?? false;
     }
+
+
+/**
+ * Return total token usage for the past 30 days.
+ *
+ * @return  array   Array of data for processing in get_system_status().
+ */
+function HookOpenai_gptAllExtra_checks() : array
+    {
+    $message['openai_gpt'] = [
+        'status' => 'OK',
+        'info' => ps_value("select sum(`count`) value from daily_stat where
+            activity_type='OpenAI Token Usage'
+        and (`year`=year(now()) or (month(now())=1 and `year`=year(now())-1))
+        and (`month`=month(now()) or `month`=month(now())-1 or (month(now())=1 and `month`=12))
+        and datediff(now(), concat(`year`,'-',lpad(`month`,2,'0'),'-',lpad(`day`,2,'0')))<=30
+            ", [], 0) // Note - limit to this month and last month before the concat to get the exact period; ensures not performing the concat on a large set of data.
+    ];
+    return $message;
+    }
