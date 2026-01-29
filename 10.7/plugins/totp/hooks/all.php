6ed94990e92828bdd9464eba403767c4c2b05bae
rcarpenter <rcarpenter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2026-01-29 11:11:42 +0000

    FIX: [10.7] Allow external access links to skip totp authentication [t38720] [CR 3724]
    git-svn-id: https://svn.resourcespace.com/svn/rs/releases@28162 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- 10.7/plugins/totp/hooks/all.php
+++ 10.7/plugins/totp/hooks/all.php
@@ -13,8 +13,16 @@ function HookTotpAllPreheaderoutput()
 {
     global $userref,$pagename, $anonymous_login, $username;
     $cookie = getval("totp", "");
+    $k = getval('k', '');
 
-    if (!in_array($pagename, array('totp', 'user_change_password')) && is_numeric($userref) && $userref > 0 && (!(isset($anonymous_login) && $username == $anonymous_login)) && $cookie != TOTP_cookie($userref)) {
+    if (
+        !in_array($pagename, array('totp', 'user_change_password')) 
+        && is_numeric($userref) && $userref > 0 
+        && (!(isset($anonymous_login) 
+        && $username == $anonymous_login)) 
+        && $cookie != TOTP_cookie($userref) 
+        && !($k != '' && !$username)
+    ) {
         redirect("plugins/totp/pages/totp.php");
     }
 }
