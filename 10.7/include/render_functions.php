84c97c1dcf88eb84a010c0065ec674dbe7aad3d4
rcarpenter <rcarpenter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2026-04-20 09:10:29 +0000

    FIX: [10.7] Allow field visibility to only depend on currently active fields during advanced search [t39023] [CR 3862]
    git-svn-id: https://svn.resourcespace.com/svn/rs/releases@28557 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- 10.7/include/render_functions.php
+++ 10.7/include/render_functions.php
@@ -79,6 +79,7 @@ function render_search_field($field,$fields,$value="",$autoupdate=false,$class="
                     $scriptconditions[$condref]["name"]                = $fields[$cf]["name"];
                     $scriptconditions[$condref]['type']                = $fields[$cf]['type'];
                     $scriptconditions[$condref]['display_as_dropdown'] = $fields[$cf]['display_as_dropdown'];
+                    $scriptconditions[$condref]['resource_types']      = $fields[$cf]['resource_types'];
                     # Get the node references of the governing field
                     $scriptconditionnodes = get_nodes($fields[$cf]['ref'], null, (FIELD_TYPE_CATEGORY_TREE == $fields[$cf]['type'] ? true : false));
 
@@ -366,10 +367,12 @@ function render_search_field($field,$fields,$value="",$autoupdate=false,$class="
                 <?php
                 }?>
 
-                // If no governing node found then disable this governed field
-                if(!field<?php echo $field['ref']; ?>valuefound)
-                {
-                field<?php echo $field['ref']; ?>visibility = false;
+                if (
+                    !field<?php echo escape($field['ref']); ?>valuefound
+                    && ((selectedtypes.includes(<?php echo json_encode($scriptcondition['resource_types']); ?>))
+                    || <?php echo escape(empty($scriptcondition['resource_types'])) ? 'true' : 'false'; ?>)
+                ) {
+                    field<?php echo escape($field['ref']); ?>visibility = false;
                 }
 
             <?php
