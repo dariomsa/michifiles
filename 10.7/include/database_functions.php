747fc05b5412d9416310bb9a2e3126c7f8189706
jbrooke <jbrooke@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2026-04-27 07:37:22 +0000

    NEW: Adding support for MySQL 9.0+ [q13563] [CR 3858]
    git-svn-id: https://svn.resourcespace.com/svn/rs/releases@28591 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- 10.7/include/database_functions.php
+++ 10.7/include/database_functions.php
@@ -904,7 +904,7 @@ function CheckDBStruct($path, $verbose = false)
                     if ($sql .= "") {
                         $sql .= ", ";
                     }
-                    $sql .= $col[0] . " " . str_replace("§", ",", $col[1]);
+                    $sql .= "`{$col[0]}` " . str_replace("§", ",", $col[1]);
 
                     if (
                         strtolower(substr($col[1], 0, 3)) == "int"
