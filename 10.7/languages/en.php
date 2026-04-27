8c271cf7c20374bebb9a94926062104473fa392f
jbrooke <jbrooke@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2026-04-27 08:53:32 +0000

    CHG: Reinstating random sort [t38894] [CR 3888]
    git-svn-id: https://svn.resourcespace.com/svn/rs/releases@28593 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- 10.7/languages/en.php
+++ 10.7/languages/en.php
@@ -549,6 +549,7 @@ $lang["popularity"] = "Popularity";
 $lang["rating"] = "Rating";
 $lang["colour"] = "Colour";
 $lang["modified"] = "Modified";
+$lang["random"] = "Random";
 $lang["jumptopage"] = "Page";
 $lang["jump"] = "Jump";
 $lang["titleandcountry"] = "Title / country";
