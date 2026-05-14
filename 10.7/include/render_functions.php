c90fec463d1a5faf81f7e68f398cf6aa10cc0f5d
rcarpenter <rcarpenter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2026-05-14 11:09:48 +0000

    FIX: [10.7] Allow value filters to display correctly and avoid being unnecessarily escaped [t39037] [CR 3905]
    git-svn-id: https://svn.resourcespace.com/svn/rs/releases@28737 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- 10.7/include/render_functions.php
+++ 10.7/include/render_functions.php
@@ -4341,6 +4341,9 @@ function display_field_data(array $field,$valueonly=false,$fixedwidth=452)
 
     if ($field['value_filter']!="")
         {
+        if ($field["type"] != FIELD_TYPE_TEXT_BOX_FORMATTED_AND_TINYMCE) {
+            $value = nl2br(escape($value));
+        }
         debug('Calling value_filter...');
         eval(eval_check_signed($field['value_filter']));
         }
@@ -4441,7 +4444,7 @@ function display_field_data(array $field,$valueonly=false,$fixedwidth=452)
         # Do not convert HTML formatted fields (that are already HTML) to HTML. Added check for extracted fields set to 
         # TinyMCE that have not yet been edited.
         if(
-            ($field["type"] != FIELD_TYPE_TEXT_BOX_FORMATTED_AND_TINYMCE && !(in_array($field['type'], $FIXED_LIST_FIELD_TYPES) && is_authenticated()))
+            ($field["type"] != FIELD_TYPE_TEXT_BOX_FORMATTED_AND_TINYMCE && !(in_array($field['type'], $FIXED_LIST_FIELD_TYPES) && is_authenticated()) && (eval_check_signed($field['value_filter'])==''))
             || ($field["type"] == FIELD_TYPE_TEXT_BOX_FORMATTED_AND_TINYMCE && $value == strip_tags($value))
             )
             {
