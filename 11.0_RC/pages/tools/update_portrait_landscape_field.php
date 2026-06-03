be54f3978dd7d420ddfbf03f385f020ceae28867
abaxter <abaxter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2026-06-03 15:45:55 +0000

    FIX: [11.0 RC] Update portrait landscape field script to work on cli [t39205]
    git-svn-id: https://svn.resourcespace.com/svn/rs/releases@28973 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- 11.0_RC/pages/tools/update_portrait_landscape_field.php
+++ 11.0_RC/pages/tools/update_portrait_landscape_field.php
@@ -3,13 +3,20 @@
 # This script is for updating the $portrait_landscape_field
 
 include "../../include/boot.php";
-include "../../include/authenticate.php";
 
-if (!checkperm("a")) {
-    exit("Permission denied");
+if ('cli' != PHP_SAPI) {
+    include "../../include/authenticate.php";
+
+    if (!checkperm("a")) {
+        exit("Permission denied");
+    }
 }
 
-include "../../include/image_processing.php";
+if (!isset($portrait_landscape_field)) {
+    exit("portrait_landscape_field not set in config");
+}
+
+include_once "../../include/image_processing.php";
 
 set_time_limit(60 * 60 * 40);
 
@@ -18,9 +25,10 @@ echo "Updating portrait_landscape_field (field $portrait_landscape_field)...";
 $rd = ps_query("SELECT ref, file_extension FROM resource WHERE has_image > 0");
 for ($n = 0; $n < count($rd); $n++) {
     $ref = $rd[$n]['ref'];
-    echo "<br />" . $ref;
+    echo ('cli' == PHP_SAPI ? "\n" : "<br />") . $ref;
     update_portrait_landscape_field($ref);
     flush();
     ob_flush();
 }
-echo "...done.";
+echo "...done." . ('cli' == PHP_SAPI ? "\n" : "<br />");
+
