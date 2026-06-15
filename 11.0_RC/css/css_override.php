ff057d9c0d30cadbd3dd28218e53c036c83b086d
jbrooke <jbrooke@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2026-06-15 10:34:01 +0000

    FIX: [11.0 RC] Improving contrast for background in dark mode [t39303]
    git-svn-id: https://svn.resourcespace.com/svn/rs/releases@29081 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- 11.0_RC/css/css_override.php
+++ 11.0_RC/css/css_override.php
@@ -58,7 +58,7 @@ if (isset($user_pref_appearance) && !(isset($high_contrast_mode) && $high_contra
             --colour-surface-mode-surface: var(--colour-neutral-850);
             --colour-surface-page-title-background: var(--colour-neutral-850);
             --colour-surface-uploader-background: var(--colour-surface-200);
-            --colour-surface-view-background: var(--colour-surface-100)
+            --colour-surface-view-background: var(--colour-surface-100);
 
             /* Colours - Typography - Dark */
             --colour-typography-50: var(--colour-neutral-1000);
@@ -95,7 +95,6 @@ if (isset($user_pref_appearance) && !(isset($high_contrast_mode) && $high_contra
 
         body, html {
             color: white;
-            background: #262626;
         }
         h1, h2 {
             color: white;
