f3544e10fa637ad29f18df8890358eaf4c444f49
tpiechowicz <tpiechowicz@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2026-06-25 10:25:29 +0000

    FIX: [11.0 RC] Adding version parameter to allow different message formatting to be supplied to earlier versions, Task #8440. [q13488]
    git-svn-id: https://svn.resourcespace.com/svn/rs/releases@29214 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- 11.0_RC/plugins/remote_assist/hooks/all.php
+++ 11.0_RC/plugins/remote_assist/hooks/all.php
@@ -3,7 +3,7 @@ function HookRemote_assistAllHomeafterwelcometext()
     {
     # Load HTML from the ResourceSpace site, passing the base URL. The base URL allows the ResourceSpace website to feed appropriate options based
     # on whether the installation is a registered trial system, user installation, etc.
-    global $baseurl;
+    global $baseurl, $productversion;
 
     ?>
     <script>
@@ -13,7 +13,7 @@ function HookRemote_assistAllHomeafterwelcometext()
             }
 
         if (jQuery('remote_assist').children().length==0 && getCookie('remote_assist_dismissed') != 'hide') {
-            jQuery('#remote_assist').load('https://www.resourcespace.com/remote_assist_plugin.php?baseurl=<?php echo base64_encode($baseurl) ?>', function () {
+            jQuery('#remote_assist').load('https://www.resourcespace.com/remote_assist_plugin.php?baseurl=<?php echo base64_encode($baseurl); ?>&version=<?php echo urlencode($productversion); ?>', function () {
                 jQuery(this).prepend('<i class="icon-info"></i>');
                 jQuery(this).append('<i class="icon-x" onclick="dismissRemoteAssist()"></i>');
                 });
