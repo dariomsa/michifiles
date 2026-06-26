5375750aed114a598a103a4126322d592f210220
acota <acota@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2026-06-26 10:53:34 +0000

    NEW: Bump minimum PHP version supported. Helps prevent logging sensitive parameters. [t39416][RC 11.0]
    git-svn-id: https://svn.resourcespace.com/svn/rs/releases@29238 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- 11.0_RC/api/index.php
+++ 11.0_RC/api/index.php
@@ -33,9 +33,6 @@ if (getval("query", "") != "") {
     $query = getval("query", "");
 }
 
-debug("API:". $query);
-
-
 # Remove the pretty, sign and authmode parameters if passed as these would not have been present when signed on the client.
 # For example, pretty JSON is just how the client wants the response back, doesn't need to to be part of the signing key process.
 parse_str($query, $query_params);
@@ -87,4 +84,4 @@ if ($function != "login") {
 
 debug("API: Executing API call");
 echo execute_api_call($query, $pretty);
-debug("API: finished execute_api_call({$query});");
+debug("API: finished execute_api_call();");
