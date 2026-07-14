4f191406eed364ef0017b4e73fde50561999733a
abaxter <abaxter@c08608d7-6e46-0410-86ca-f2a6f1370df5>
2026-07-14 13:58:14 +0000

    FIX: [11.0][CR4062] Revert changes to pages/ajax/message.php from r29305 [t39494]
    git-svn-id: https://svn.resourcespace.com/svn/rs/releases@29347 c08608d7-6e46-0410-86ca-f2a6f1370df5

--- 11.0/pages/ajax/message.php
+++ 11.0/pages/ajax/message.php
@@ -212,11 +212,9 @@ if (basename(__FILE__) == basename($_SERVER['PHP_SELF'])) {
                         researchcount = parseInt(messages[messagecount]['researchcount']);
                         admintotalcount = admintotalcount + researchcount;
                     }
-                    if (typeof(messages[messages.length - 1]['failedjobcount']['user']) !== 'undefined') {
+                    if (typeof(messages[messages.length - 1]['failedjobcount']) !== 'undefined') {
                         userfailedjobcount = parseInt(messages[messagecount]['failedjobcount']['user']);
                         usertotalcount     = usertotalcount + userfailedjobcount;
-                    }
-                    if (typeof(messages[messages.length - 1]['failedjobcount']['all']) !== 'undefined') {
                         failedjobcount     = parseInt(messages[messagecount]['failedjobcount']['all']);
                         admintotalcount = admintotalcount + failedjobcount;
                     }
